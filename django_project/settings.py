95555fa8d1d71a63cfd37d499b284bfd9aeb903e
Ognev Ilya <kalevala17@yandex.ru>
2026-04-21 00:27:22 +0300

    Fix for Railway (22/04/2026)

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,17 +76,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
 # Database
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 # Password validation
