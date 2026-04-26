caa99b75147dfd44c123fdfc1638d1f7205506a0
Ognev Ilya <kalevala17@yandex.ru>
2026-04-26 20:41:14 +0300

    Fix for Railway (26/04/2026)

--- main/views.py
+++ main/views.py
@@ -171,6 +171,18 @@ def process_video_task(task_id, tmp_path, original_filename):
 
         # 1. Конвертация
         save_task_status(task_id, {'status': 'converting', 'message': 'Конвертация видео...'})
+        for folder in ['tmp', 'processed_videos', 'results']:
+            folder_path = os.path.join(settings.MEDIA_ROOT, folder)
+            if os.path.exists(folder_path):
+                for filename in os.listdir(folder_path):
+                    file_path = os.path.join(folder_path, filename)
+                    try:
+                        if os.path.isfile(file_path):
+                            os.unlink(file_path)
+                        elif os.path.isdir(file_path):
+                            shutil.rmtree(file_path)
+                    except Exception as e:
+                        print(f"Не удалось удалить {file_path}: {e}")
         base, _ = os.path.splitext(tmp_path)
         converted_path = base + '_converted.mp4'
         print(f"[{time.time()}] Конвертация {tmp_path} -> {converted_path} через ffmpeg")
